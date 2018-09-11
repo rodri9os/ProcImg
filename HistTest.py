97fa1ccb6fa7d4fc94fbac213a8b10e430d35025
LukasPol <lukaspaespol@gmail.com>
2018-09-11 19:04:44 -0300

    testando as imagens

--- HistTest.py
+++ HistTest.py
@@ -12,7 +12,7 @@ from matplotlib import pyplot as plt
 from scipy.misc import imread
 
 #pegando somente uma das cores da imagem
-img = cv2.imread('Imagens/dog.jpg',0)
+img = cv2.imread('training_set/dogs/dog.4.jpg',0)
 #plt.plot([0:100],100, linestyle='-')  # solid
 #x = np.linspace(0, 163)
 #plt.plot([0 100], [0 100])
@@ -24,10 +24,10 @@ plt.imshow(img)
 histr = cv2.calcHist([img],[0],None,[256],[0,256])
 plt.figure()
 plt.plot(histr,'b')
-plt.xlim([0,256])
+plt.xlim([100,256])
 plt.show()
 
-thrs, dst  = cv2.threshold(img, 10, 255, cv2.THRESH_TRUNC)
+thrs, dst  = cv2.threshold(img, 100, 200, cv2.THRESH_TRUNC)
 plt.imshow(dst,'gray')
 
 #img1 = cv2.imread('Imagens/dog.jpg',0)
