63f7bb17d18890e40177faf81fa46ef0ebb84af2
LukasPol <lukaspaespol@gmail.com>
2018-09-11 19:21:57 -0300

    contraste

---
+++ hist_contraste.py
@@ -0,0 +1,19 @@
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+img = cv2.imread('training_set/dogs/dog.11.jpg', 0)
+
+hist, bins = np.histogram(img.flatten(), 256, [0, 256])
+
+
+contraste = hist.cumsum()
+contraste_normalized = contraste * hist.max() / contraste.max()
+
+plt.plot(contraste_normalized, color='b')
+# plt.hist(1000, 256, [0, 256], color='r')
+plt.hist(img.flatten(), 256, [0, 256], color='r')
+plt.xlim([0, 256])
+plt.legend(('contraste', 'histogram'), loc='upper left')
+
+plt.show()
