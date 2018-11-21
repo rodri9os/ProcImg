5a5aebe796e3f871649a2d0a29d0118c3f202b84
Lukas Pol dos Santos Paes <lukas.pol@academico.ifrn.edu.br>
2018-11-21 14:14:17 -0300

    imagem

--- algoritmo_binarização.py
+++ algoritmo_binarização.py
@@ -1,47 +1,10 @@
 import cv2
 import numpy as np
+from matplotlib import pyplot as plt
+#from skimage.filters import theshold_mininum
 
-img = cv2.imread('training_set/dogs/dog.14.jpg',0)
+def binarizazao(img ,threshold):
 
-tam = img.shape
-
-lin = tam[0]
-col = tam[1]
-# LINHA E COLUNAS
-
-threshold = int(input('Número do threshold: '))
-
-img_nova=[]
-
-for i in range(lin):
-    linha=[]
-    for j in range(col):
-        if threshold >= img[i][j]:
-            linha.append(0)
-        else:
-            linha.append(255)
-    img_nova.append(linha)
-
-img_nova = np.array(img_nova)
-
-cv2.imwrite('img_nova.jpg', img_nova)
-
-img_nova = cv2.imread('img_nova.jpg',0)
-
-# juntando as imagens
-asDuas = np.hstack((img,img_nova))
-
-cv2.imshow('as duas',asDuas)
-
-cv2.imshow('img Original',img)
-cv2.imshow('Binarizaçao',img_nova)
-
-k = cv2.waitKey(0)
-
-
-## Em forma de função
-## Passar a imagem ao chama-la
-def binarizazao(img):
     tam = img.shape
 
     lin = tam[0]
@@ -65,3 +28,60 @@ def binarizazao(img):
     img_nova = cv2.imread('img_nova.jpg',0)
 
     return img_nova
+
+
+
+
+img = cv2.imread('training_set/dogs/dog.14.jpg',0)
+
+threshold = int(input('Número do threshold: '))
+
+img_nova = binarizazao(img,threshold)
+
+plt.hist(img.ravel(),bins=256)
+
+plt.axvline(threshold, color = 'r')
+
+cv2.imwrite('img_nova.jpg', img_nova)
+
+img_nova = cv2.imread('img_nova.jpg',0)
+
+# juntando as imagens
+asDuas = np.hstack((img,img_nova))
+
+cv2.imshow('as duas',asDuas)
+
+#cv2.imshow('img Original',img)
+#cv2.imshow('Binarizaçao',img_nova)
+
+#plt.imshow(histograma,'a')
+plt.show()
+
+
+k = cv2.waitKey(0)
+
+
+## Em forma de função
+## Passar a imagem ao chama-la
+""""
+tam = img.shape
+
+lin = tam[0]
+col = tam[1]
+# LINHA E COLUNAS
+
+
+
+img_nova=[]
+
+for i in range(lin):
+    linha=[]
+    for j in range(col):
+        if threshold >= img[i][j]:
+            linha.append(0)
+        else:
+            linha.append(255)
+    img_nova.append(linha)
+
+img_nova = np.array(img_nova)
+"""
