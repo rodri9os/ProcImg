2a706054f373e67984eaa6a7aa4943c072dce7e4
rodri9os <rodrigos@oi.com.br>
2018-11-30 07:30:40 -0300

    vizinhos

--- algoritmos_criados.py
+++ algoritmos_criados.py
@@ -19,11 +19,11 @@ def binarizacao(img, threshold):
                 linha.append(255)
         img_nova.append(linha)
 
-    img_nova = np.array(img_nova)
+    img_nova = np.array(img_nova, np.uint8)
 
-    cv2.imwrite('img_nova.jpg', img_nova)
+    #cv2.imwrite('img_nova.jpg', img_nova)
 
-    img_nova = cv2.imread('img_nova.jpg', 0)
+    #img_nova = cv2.imread('img_nova.jpg')
 
     return img_nova
 
@@ -50,3 +50,93 @@ def threshold (img,threshold):
     cv2.imwrite('img_nova.jpg', img_nova)
 
     img_nova = cv2.imread('img_nova.jpg', 0)
+    
+def vizinho (img_bi,i,j):
+    
+    
+    vizinho = {}
+    
+    tam = img_bi.shape
+
+    lin = tam[0]
+    col = tam[1]
+
+
+    if i == 0 and j == 0:
+        vizinho[1] = None
+        vizinho[2] = None
+        vizinho[3] = None
+        vizinho[4] = None
+        vizinho[5] = img_bi[i,j+1]
+        vizinho[6] = None
+        vizinho[7] = img_bi[i+1,j]
+        vizinho[8] = img_bi[i+1,j+1]
+
+    elif i == 0 and j == col:
+        vizinho[1] = None
+        vizinho[2] = None
+        vizinho[3] = None
+        vizinho[4] = img_bi[i,j-1]
+        vizinho[5] = None
+        vizinho[6] = img_bi[i+1,j-1]
+        vizinho[7] = img_bi[i+1,j]
+        vizinho[8] = None
+
+
+    if i == 0 :
+        vizinho[1] = None
+        vizinho[2] = None
+        vizinho[3] = None
+        vizinho[4] = img_bi[i,j-1]
+        vizinho[5] = img_bi[i,j+1]
+        vizinho[6] = img_bi[i+1,j-1]
+        vizinho[7] = img_bi[i+1,j]
+        vizinho[8] = img_bi[i+1,j+1]
+
+       
+    elif i == lin and j == 0 :
+        vizinho[1] = None
+        vizinho[2] = img_bi[i-1,j]
+        vizinho[3] = img_bi[i-1,j+1]
+        vizinho[4] = None
+        vizinho[5] = img_bi[i,j+1]
+        vizinho[6] = None
+        vizinho[7] = None
+        vizinho[8] = None
+
+        
+    elif i == lin and j == col:
+        vizinho[1] = img_bi[i-1,j-1]
+        vizinho[2] = img_bi[i-1,j]
+        vizinho[3] = None
+        vizinho[4] = img_bi[i,j-1]
+        vizinho[5] = None
+        vizinho[6] = None
+        vizinho[7] = None
+        vizinho[8] = None
+
+    elif j == col:
+        vizinho[1] = img_bi[i-1,j-1]
+        vizinho[2] = img_bi[i-1,j]
+        vizinho[3] = img_bi[i-1,j+1]
+        vizinho[4] = img_bi[i,j-1]
+        vizinho[5] = img_bi[i,j+1]
+        vizinho[6] = None
+        vizinho[7] = None
+        vizinho[8] = None
+        
+    else:    
+        vizinho[1] = img_bi[i-1,j-1]
+        vizinho[2] = img_bi[i-1,j]
+        vizinho[3] = img_bi[i-1,j+1]
+        vizinho[4] = img_bi[i,j-1]
+        vizinho[5] = img_bi[i,j+1]
+        vizinho[6] = img_bi[i+1,j-1]
+        vizinho[7] = img_bi[i+1,j]
+        vizinho[8] = img_bi[i+1,j+1]
+        
+    return vizinho
+    
+    
+    
+
--- cont_objetos.py
+++ cont_objetos.py
@@ -1,18 +1,34 @@
 import cv2
 import numpy as np
-import algoritmo_binarização as bn
+import algoritmos_criados as bn
 
 img = cv2.imread('training_set/dogs/dog.14.jpg',0)
+vizinho = {}
 
 tam = img.shape
 
 lin = tam[0]
 col = tam[1]
 
-img_bi = bn.binarizazao(img) # preto e branco
+img_bi = bn.binarizacao(img,114) # preto e branco
+#cv2.imshow('Figura',img_bi)
+
+#k = cv2.waitKey(0)
+
+vizinho = bn.vizinho(img_bi,356,66)
+
+
+i = 356
+j = 66
+
 
 for i in range(lin):
     for j in range(col):
         print()
         ### Não acabado
 
+for i in range(lin):
+    for j in range(col):
+        if img_bi[i,j] == 1: 
+            print("i = %d, j = %d" % (i,j))
+            break
